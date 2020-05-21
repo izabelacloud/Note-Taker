fd96d196d4ff6922e67ae7f76e3407e48e6342c2
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-21 16:43:18 -0700

    adding Heroku

--- server.js
+++ server.js
@@ -20,6 +20,43 @@ app.use(express.static('public'));
 const { notes } = require('./db/db.json');
 
 
+//get request to get all the notes and return all saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+    // console.log(notes)
+});
+
+
+// //post request to server to accept data to be used or stored server-side
+// app.post('/notes', (req, res) => {
+//     // set id based on what the next index of the array will be 
+//     req.body.id = notes.length.toString(); 
+
+//     // if any data in req.body is incorrect, send error
+//     if (!validateNote(req.body)) {
+//         res.status(400).send('The note is not properly formatted.'); 
+//     } else {
+//     // add note to json file and notes array in this function 
+//     const note = createNewNote(req.body, notes); 
+
+//     res.json(note); 
+//     }
+// });
+
+
+
+
+// //route to notes.html
+// app.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/notes.html'));
+//   });
+
+// //route to index.html
+// app.get('/', (req, res) => {
+// res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+
 // listener method to our servers 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
