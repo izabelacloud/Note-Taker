3015525c1b82345893d48b5d2e4c8be987626932
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-21 16:54:04 -0700

    updating functions in server.js

--- server.js
+++ server.js
@@ -20,6 +20,38 @@ app.use(express.static('public'));
 const { notes } = require('./db/db.json');
 
 
+function createNewNote (body, notesArray) {
+    const note = body; 
+    notesArray.push(note); 
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        // converting JS array to JSON 
+        JSON.stringify({ notes : notesArray }, null, 2)
+    
+    );
+    // return finished code to post route for response
+    return note; 
+}
+
+
+//function to validate the notes
+function validateNote (note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false; 
+    }
+    if (!note.text || typeof note.text !== "string") {
+        return false;
+    }
+    if (!note.id || typeof note.id !== "number") {
+        return false;
+      }
+
+    return true;   
+}
+
+
+
+
 //get request to get all the notes and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
     res.json(notes);
@@ -27,34 +59,35 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-// //post request to server to accept data to be used or stored server-side
-// app.post('/notes', (req, res) => {
-//     // set id based on what the next index of the array will be 
-//     req.body.id = notes.length.toString(); 
+//post request to server to accept data to be used or stored server-side
+app.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be 
+    req.body.id = notes.length.toString(); 
 
-//     // if any data in req.body is incorrect, send error
-//     if (!validateNote(req.body)) {
-//         res.status(400).send('The note is not properly formatted.'); 
-//     } else {
-//     // add note to json file and notes array in this function 
-//     const note = createNewNote(req.body, notes); 
+    // if any data in req.body is incorrect, send error
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.'); 
+    } else {
+    // add note to json file and notes array in this function 
+    const note = createNewNote(req.body, notes); 
 
-//     res.json(note); 
-//     }
-// });
-
-
+    res.json(note); 
+    }
+});
 
 
-// //route to notes.html
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/notes.html'));
-//   });
+//route to index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+    });
 
-// //route to index.html
-// app.get('/', (req, res) => {
-// res.sendFile(path.join(__dirname, './public/index.html'));
-// });
+
+
+//route to notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
 
 
 // listener method to our servers 
